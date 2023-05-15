2b33e93e88520d4588cf61ee48ffd1a83feb6656
harris <harryaamir073@gmail.com>
2023-05-15 19:44:53 +0500

    V3

--- backend/controllers/Resturant.js
+++ backend/controllers/Resturant.js
@@ -1,13 +1,63 @@
 const restaurantModel = require("../models/restaurants");
 
 let createRestaurant =async (req, res) => {
-    const restaurant = new restaurantModel(req.body);
-    await restaurant.save();
+    const rest={
+        "restaurantdescription": req.body.restaurantdescription,
+        "restaurantname": req.body.restaurantname,
+        "restaurantpassword": req.body.restaurantpassword,
+        "restaurantstatus": req.body.restaurantstatus,
+        "restauranttype": req.body.restauranttype,
+        "restaurantusername":req.body.restaurantusername,
+        "restaurantlogo": req.body.restaurantlogo
+    }
+    
+    const restaurant = new restaurantModel(rest);
+    console.log(restaurant)
+    await restaurant.save().catch(err=>{console.log(err)});
     res.json(restaurant);}
 
-
-
+let deleteRestaurant = async (req, res) => {
+        const restaurantID = req.params.id;
+        restaurantModel.findByIdAndDelete(restaurantID).then((deletedRestaurant)=> {
+          if (deletedRestaurant) {
+          res.json(deletedRestaurant);
+        } else {
+          res.status(404).json({ error: 'Restaurant not found' })
+        }}).catch(error=>{
+        res.status(500).json({ error: 'An error occurred while deleting the restaurant' })});
+        }
+    let editRestaurant = async (req, res) => {
+            const restaurantID = req.body._id;
+            const updatedData={
+                "restaurantdescription": req.body.restaurantdescription,
+                "restaurantname": req.body.restaurantname,
+                "restaurantpassword": req.body.restaurantpassword,
+                "restaurantstatus": req.body.restaurantstatus,
+                "restauranttype": req.body.restauranttype,
+                "restaurantusername":req.body.restaurantusername,
+                "restaurantlogo": req.body.restaurantlogo
+            }
+          
+            restaurantModel.findByIdAndUpdate(restaurantID, updatedData, { new: true })
+              .then((updatedRestaurant) => {
+                if (updatedRestaurant) {
+                  res.json(updatedRestaurant);
+                } else {
+                  res.status(404).json({ error: 'Restaurant not found' });
+                }
+              })
+              .catch((error) => {
+                res.status(500).json({ error: 'An error occurred while updating the restaurant' });
+              });
+          };
+        let getAllRestaurants = async (req, res) => {
+            restaurantModel.find().then(res=>{res.json(restaurants)}).catch(error =>{
+              res.status(500).json({ error: 'An error occurred while fetching the restaurants' })});
+            }          
+          
 module.exports = {
    createRestaurant,
-  
+   deleteRestaurant,
+   editRestaurant,
+   getAllRestaurants
 }
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -4,6 +4,9 @@ const {editStore} = require("../controllers/Store")
 const {deleteStore} = require("../controllers/Store")
 const {viewAllStores} = require("../controllers/Store")
 const {createRestaurant} = require("../controllers/Resturant")
+const {deleteRestaurant} = require("../controllers/Resturant")
+const {editRestaurant} = require("../controllers/Resturant")
+const {getAllRestaurants} = require("../controllers/Resturant")
 const AdminRouter = require("express").Router();
 
 AdminRouter.get("/viewAllStores",viewAllStores )
@@ -11,6 +14,9 @@ AdminRouter.post("/addNewStore",createStore )
 AdminRouter.put("/editStore",editStore )
 AdminRouter.delete("/deleteStore:id",deleteStore )
 AdminRouter.post("/addNewResturant",createRestaurant )
-
+AdminRouter.delete("/deleteRestaurant:id",deleteRestaurant )
+AdminRouter.post("/editRestaurant",editRestaurant )
+AdminRouter.get("/getAllRestaurants",getAllRestaurants)
 
 module.exports = AdminRouter;
+
